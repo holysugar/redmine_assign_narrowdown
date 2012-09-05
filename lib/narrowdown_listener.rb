c7e729638251a2b96535e3d67b36e80434b74fdf
HORII Keima <holysugar@gmail.com>
2012-09-05 19:53:13 +0900

    fix to work
    option に display:none 効かないので DOM 的にやっつけた

--- lib/narrowdown_listener.rb
+++ lib/narrowdown_listener.rb
@@ -2,7 +2,9 @@ class NarrowdownListener < Redmine::Hook::ViewListener
   def view_issues_edit_notes_bottom(*args)
 js=<<'JS'
 (function(){
-  var e = $("issue_assigned_to_id");
+  var id_enabled = "issue_assigned_to_id";
+  var id_disabled = "issue_assigned_to_id_disabled";
+  var e = document.getElementById(id_enabled);
   if(e){
     var useridlist={};
     $$("a[href*=users]").each(function(a){
@@ -15,21 +17,39 @@ js=<<'JS'
     e.parentNode.insertBefore(is,e.nextSibling);
     var label = document.createElement("span");
     label.appendChild(document.createTextNode("関係者のみ"));
+    label.for = "only-concerned";
     label.setAttribute("style","font-size:80%;cursor:pointer");
     is.id = label.for;
     e.parentNode.insertBefore(label,is.nextSibling);
-    $(label).observe("click",function(){
-      is.click();
-    });
-    $(is).observe("change",function(){
-      for(var i=0;i<e.options.length;i++){
-        var o = e.options[i];
+    
+    var copiedselect = e.cloneNode(true);
+    copiedselect.id = id_disabled;
+    copiedselect.style.display = "none";
+    var options = copiedselect.options;
+    
+    for (var i = 0, l = options.length; i < l; i++) {
+      var o = options[i];
+      console.log(o);
+      if (!useridlist[o.value]){
+        jQuery(o).attr("data-toremove", '1');
+      }
+    }
+    e.parentNode.insertBefore(copiedselect, e.nextSibling);
+    jQuery('option[data-toremove=1]').remove();
 
-        if(is.checked && !useridlist[o.value]){
-          o.style.display="none";
-        }else{
-          o.style.display="";
-        }
+    $(is).on("change", function(ev) {
+      if (ev.target.checked) {
+        copiedselect.id = id_enabled;
+        copiedselect.style.display = "";
+        e.id = id_disabled;
+        e.style.display = "none";
+        console.log('enabled');
+      } else {
+        copiedselect.id = id_disabled;
+        copiedselect.style.display = "none";
+        e.id = id_enabled;
+        e.style.display = "";
+        console.log('disabled');
       }
     });
   }
